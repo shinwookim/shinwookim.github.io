7d51c556a76c6eb5a765c3d1e6a2d0366cc983bc
Shinwoo Kim <shinwookim@pitt.edu>
2026-08-18 12:00:34 +0900

    content: post -> blog

--- assets/js/nav_toggle.js
+++ assets/js/nav_toggle.js
@@ -1,6 +1,7 @@
 const btn = document.getElementById("nav-toggle");
 const menu = document.getElementById("nav-menu");
 
-btn.addEventListener("click", () => {
-	menu.classList.toggle("hidden");
-});
+if (menu != null)
+	btn.addEventListener("click", () => {
+		menu.classList.toggle("hidden");
+	});
